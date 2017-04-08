b49b8d11c24392874331d00a174062961b7173fc
baumy1 <baumy@hotmail.com>
2017-04-08 16:00:14 +1000

    Update sketch.js

--- public/sketch.js
+++ public/sketch.js
@@ -8,7 +8,6 @@ document.getElementById('btn').addEventListener("click", sendmsg);
 
 function setup() {
   createCanvas(400, 400);
-  bg = loadImage("image.png");
   // Start a socket connection to the server
   // Some day we would run this server somewhere else
   socket = io.connect('https://nodejsmessaging.herokuapp.com/');
