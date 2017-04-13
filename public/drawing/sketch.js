8a264675d5387b9ffa0e9b4310943ae19dc81c63
baumy1 <baumy@hotmail.com>
2017-04-13 11:09:18 +1000

    Update sketch.js

--- public/drawing/sketch.js
+++ public/drawing/sketch.js
@@ -1,20 +1,20 @@
-// Keep track of our socket connection
 var socket;
 var fileName;
+var table;
+var circles;
+
+function preload() {
+  table = loadTable("data/data.csv", "header");
+}
 
 function setup() {
+  loadData();
   createCanvas(1000, 563);
-  // Start a socket connection to the server
-  // Some day we would run this server somewhere else
   socket = io.connect('https://nodejsmessaging.herokuapp.com');
-  // We make a named event called 'mouse' and write an
-  // anonymous callback function
-  
+ 
   socket.on('mouse',
-    // When we receive data
     function(data) {
       console.log("Got: " + data.x + " " + data.y);
-      // Draw a blue circle
       fill(0,0,255);
       noStroke();
       ellipse(data.x, data.y, 20, 20);
@@ -26,11 +26,9 @@ function draw() {
 }
 
 function mouseDragged() {
-  // Draw some white circles
   fill(255);
   noStroke();
   ellipse(mouseX,mouseY,20,20);
-  // Send the mouse coordinates
   sendmouse(mouseX,mouseY);
 }
 
@@ -47,4 +45,22 @@ function sendmouse(xpos, ypos) {
 
   // Send that object to the socket
   socket.emit('mouse',data);
+}
+
+function loadData() {
+  // Load CSV file into a Table object
+  // "header" option indicates the file has a header row
+
+  // The size of the array of Bubble objects is determined by the total number of rows in the CSV
+  circles = []; 
+
+  // You can access iterate over all the rows in a table
+  for (var i = 0; i < table.getRowCount(); i++) {
+    var row = table.getRow(i);
+    // You can access the fields via their column name (or index)
+    var x = row.get("x");
+    var y = row.get("y");
+    var c = row.get("colour");
+    circles[i] = new Bubble(x, y, c);
+  }
 }
